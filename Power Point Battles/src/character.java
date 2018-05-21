413881e1d2cb97e9a94b4f14a5947a588dbcadaa
jhockel <jhockel@gmail.com>
2018-05-21 01:34:44 -0700

    adding LIBXL

---
+++ Power Point Battles/src/character.java
@@ -0,0 +1,16 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+public class character {
+	
+	
+	static ArrayList<Integer> balancebone = null;
+	ArrayList<Integer> statbone = null;
+	
+	
+	
+	public character() {
+		balancebone = new ArrayList<Integer>();
+	}
+
+}
