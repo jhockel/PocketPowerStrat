2204dc202840861d073c888ef5c0040866734edb
jhockel <jhockel@gmail.com>
2018-06-22 00:44:42 -0700

    game board draws

--- stateofpowaa/excelio.cpp
+++ stateofpowaa/excelio.cpp
@@ -24,15 +24,19 @@ using namespace libxl;
     
 // // LibXL Constants:
 const string GameBoard("Gameplay/game.xlsx");
+const string GameSheet("GameBoard");
 Book* gameBook = NULL;
 Sheet* gamePage = NULL;
+
+#define TLcR(RY,RX) (6+RY*8)
+#define TLcC(RY,RX) (RY%2 ? 8+RX*4 : 6+RX*4)
 
 // Excel Interaction Implimentations
 
 int libxlio::openGameBook() {
     gameBook = xlCreateXMLBook();
-    if(!gameBook->load(GameBoard.c_str())) return 0;
-    gamePage = gameBook->getSheet(0);
+    if(!gameBook) return 0;
+    gamePage = gameBook->insertSheet(0,GameSheet.c_str());
     if(!gamePage) return 0;
     return 1;
 }
@@ -63,10 +67,59 @@ void libxlio::drawboard() {
     Format* topB = gameBook->addFormat();
     topB->setBorderTop(BORDERSTYLE_THICK);
     Format* leftB = gameBook->addFormat();
-    topB->setBorderLeft(BORDERSTYLE_THICK);
+    leftB->setBorderLeft(BORDERSTYLE_THICK);
+    Format* rightB = gameBook->addFormat();
+    rightB->setBorderRight(BORDERSTYLE_THICK);
+    Format* bottomB = gameBook->addFormat();
+    bottomB->setBorderBottom(BORDERSTYLE_THICK);
     
-    gamePage->writeBlank(3,3,topB);
-    gamePage->writeBlank(3,4,leftB);
+    Format* TLc = gameBook->addFormat();
+    TLc->setBorderTop(BORDERSTYLE_THICK);
+    TLc->setBorderLeft(BORDERSTYLE_THICK);
+    Format* TRc = gameBook->addFormat();
+    TRc->setBorderTop(BORDERSTYLE_THICK);
+    TRc->setBorderRight(BORDERSTYLE_THICK);
+    Format* BLc = gameBook->addFormat();
+    BLc->setBorderBottom(BORDERSTYLE_THICK);
+    BLc->setBorderLeft(BORDERSTYLE_THICK);
+    Format* BRc = gameBook->addFormat();
+    BRc->setBorderBottom(BORDERSTYLE_THICK);
+    BRc->setBorderRight(BORDERSTYLE_THICK);
     
+    gamePage->setCol(0,30,5.5);
+    // First, draw outer boarder 
+    // Top Left corner: (8,4)
+    // 5.5x5 spaces:
+    // 22x40 cells
+    
+    gamePage->writeBlank(2,4,TLc);
+    gamePage->writeBlank(2,25,TRc);
+    gamePage->writeBlank(41,4,BLc);
+    gamePage->writeBlank(41,25,BRc);
+    
+    for(int i = 5; i<25; i++) {
+        gamePage->writeBlank(2,i,topB);
+        gamePage->writeBlank(42,i,topB);
+    }
+    for(int i = 3; i<41; i++) {
+        gamePage->writeBlank(i,4,leftB);
+        gamePage->writeBlank(i,26,leftB);
+    }
+    
+    for(int x = 0; x<4; x++)
+        for(int y = 0; y<4; y++) {
+            gamePage->writeBlank(TLcR(x,y),TLcC(x,y),TLc);
+            gamePage->writeBlank(TLcR(x,y),TLcC(x,y)+3,TRc);
+            gamePage->writeBlank(TLcR(x,y)+7,TLcC(x,y),BLc);
+            gamePage->writeBlank(TLcR(x,y)+7,TLcC(x,y)+3,BRc);
+            for(int z = 1; z<3; z++){
+                gamePage->writeBlank(TLcR(x,y),TLcC(x,y)+z,topB);
+                gamePage->writeBlank(TLcR(x,y)+7,TLcC(x,y)+z,bottomB);
+            }
+            for(int z = 1; z<7; z++){
+                gamePage->writeBlank(TLcR(x,y)+z,TLcC(x,y),leftB);
+                gamePage->writeBlank(TLcR(x,y)+z,TLcC(x,y)+3,rightB);
+            }
+        }
     
 }
